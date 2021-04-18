dd31d9a86cb735b467f0c7689a9a9e041fdf4939
shuby mao <le-li.mao@mail.mcgill.ca>
2021-04-17 21:14:20 -0400

    modified 404 page

--- src/components/page.tsx
+++ src/components/page.tsx
@@ -8,11 +8,12 @@ export interface PageProps {
 
 const Page: React.FC<PageProps> = (props) => {
   const { children } = props;
+  const padTop = { paddingTop: '40px' };
   return (
     <div className="w-full">
       <Header />
-      <div className="w-full min-h-screen flex flex-col justify-between" style={{ paddingTop: '40px' }}>
-        <div className="pb-16">{children}</div>
+      <div className="w-full min-h-screen flex flex-col justify-between" style={padTop}>
+        <div className="flex-grow pb-16 block">{children}</div>
         <Footer />
       </div>
     </div>
