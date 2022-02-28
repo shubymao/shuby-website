cf492efe30258672cd249931ef46012ed60bd3e1
shubymao <le-li.mao@mail.mcgill.ca>
2022-02-27 20:05:46 -0800

    fixed a bug

--- tests/integration.spec.ts
+++ tests/integration.spec.ts
@@ -2,34 +2,34 @@ import { test } from '@playwright/test';
 import { visitAndSnapShot } from './test-helper';
 
 test('Home Page', async ({ context, baseURL }) => {
-  await visitAndSnapShot(context, baseURL, 'home.jpg', 2000);
+  await visitAndSnapShot(context, baseURL, 'home.png', 2000);
 });
 
 test('Software Page', async ({ context, baseURL }) => {
-  await visitAndSnapShot(context, `${baseURL}/software`, 'software.jpg');
+  await visitAndSnapShot(context, `${baseURL}/software`, 'software.png');
 });
 
 test('System Page', async ({ context, baseURL }) => {
-  await visitAndSnapShot(context, baseURL + '/system', 'system.jpg');
+  await visitAndSnapShot(context, baseURL + '/system', 'system.png');
 });
 
 test('Contact Page', async ({ context, baseURL }) => {
-  await visitAndSnapShot(context, baseURL + '/contact', 'contact.jpg');
+  await visitAndSnapShot(context, baseURL + '/contact', 'contact.png');
 });
 
 test('Projects Pages', async ({ context, baseURL }) => {
-  await visitAndSnapShot(context, baseURL + '/projects', 'projects.jpg');
+  await visitAndSnapShot(context, baseURL + '/projects', 'projects.png');
 });
 
 test('Notes Pages', async ({ baseURL, context }) => {
-  await visitAndSnapShot(context, baseURL + '/notes', 'notes.jpg');
+  await visitAndSnapShot(context, baseURL + '/notes', 'notes.png');
 });
 
 test('Sample Note Page', async ({ baseURL, context }) => {
   await visitAndSnapShot(
     context,
     baseURL + '/notes/sample-note',
-    'sample-note.jpg',
+    'sample-note.png',
   );
 });
 
@@ -37,6 +37,6 @@ test('Sample Project Page', async ({ baseURL, context }) => {
   await visitAndSnapShot(
     context,
     baseURL + '/projects/sample-project',
-    'sample-project.jpg',
+    'sample-project.png',
   );
 });
